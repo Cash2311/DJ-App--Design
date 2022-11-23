aa1c799afb1bf1f7a9fff2f68fdc3c5d34e4f379
Cash2311 <86266624+Cash2311@users.noreply.github.com>
2022-11-22 19:46:25 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX +"leftWristY =" + leftWristY);
@@ -45,8 +47,19 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 500);
-}
+   fill("#FF0000");
+   stroke("#FF0000");
+if(scoreleftWrist > 0.2)
+{
 
+  circle(leftWristX, leftWristY, 20);
+  InNumberleftWristY = Number(leftWristY);
+  remove_decimals = floor(InNumberleftWristY);
+  volume = remove_decimals/500;
+  document.getElementById("volume").innerHTML = "Volume = " + volume;
+song.setVolume(volume);
+}
+}
 function play()
 {
 song.play();
